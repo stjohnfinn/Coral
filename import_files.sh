7d10f4a0a21980cbfba502f15e1b8212afe0497f
Finn St John <stjohn.finn@gmail.com>
2024-09-28 20:05:49 -0700

    add a bunch of awesome things

--- import_files.sh
+++ import_files.sh
@@ -8,7 +8,7 @@ set -eou pipefail
 #   This script "imports" configuration files from the current machine into this 
 # project. All files are listed below. For example, if I am using my development
 # machine and I decide to update the Codium config files, I can just run this 
-# script and it will import ~/.config/VSCodium/User/settings.json to the correct
+# script and it will import $HOME/.config/VSCodium/User/settings.json to the correct
 # location.
 
 ###################
@@ -19,56 +19,56 @@ set -eou pipefail
 # 
 # the first string in each element is the source path (the path on the local machine)
 # the second string in each element is the destination path (the path in the appropriate <role>/files directory)
+#   the destination path must be the full path, OR YOU DIE
 files=(
-  "~/.config/VSCodium/User/settings.json roles/codium_config/files/"
-  "~/.config/VSCodium/User/keybindings.json roles/codium_config/files/"
+  "$HOME/.config/VSCodium/User/settings.json roles/codium_config/files/settings.json"
+  "$HOME/.config/VSCodium/User/keybindings.json roles/codium_config/files/keybindings.json"
 
-  "~/.bashrc roles/dotfiles/files/"
-  "~/.inputrc roles/dotfiles/files/"
-  "~/.vimrc roles/dotfiles/files/"
-  "~/.zshrc roles/dotfiles/files/"
+  "$HOME/.bashrc roles/dotfiles/files/.bashrc"
+  "$HOME/.inputrc roles/dotfiles/files/.inputrc"
+  "$HOME/.vimrc roles/dotfiles/files/.vimrc"
+  "$HOME/.zshrc roles/dotfiles/files/.zshrc"
 
-  "~/.config/alacritty.yml roles/dotfiles/files/"
-  "~/.config/gammastep.conf roles/dotfiles/files/"
+  "$HOME/.config/alacritty.yml roles/dotfiles/files/alacritty.yml"
+  # "$HOME/.config/gammastep.conf roles/dotfiles/files/gammastep.conf"
 )
 
-for file in "${files[0]}"; do
+################################################################################
+# algorithm
+################################################################################
+
+for file in "${files[@]}"; do
   # split the string into source and destination
   source_path=$(echo "$file" | awk '{print $1}')
+  source_path_absolute=$(realpath $source_path)
   destination_path=$(echo "$file" | awk '{print $2}')
 
   # check if the source file exists
-  if [ ! -f "$source_path" ]; then
-    echo "Error: Source file '$source_path' does not exist. Skipping..."
+  if [ ! -f "$source_path_absolute" ]; then
+    echo "Error: Source file '$source_path_absolute' does not exist. Skipping..."
+    file $source_path_absolute
     continue
   fi
 
   # check if the destination file exists
   if [ ! -f "$destination_path" ]; then
     echo "Warning: Destination file '$destination_path' does not exist."
-    echo "Do you want to proceed with copying '$source_path' to '$destination_path'? (y/N)"
+    echo "Do you want to proceed with copying '$source_path_absolute' to '$destination_path'? (y/N)"
     read -r user_input
 
     if [ "$user_input" != "y" ]; then
-      echo "Skipping copy for '$source_path'..."
+      echo "Skipping copy for '$source_path_absolute'..."
       continue
     fi
   fi
 
   # perform the copy operation
-  cp "$source_path" "$destination_path"
+  cp "$source_path_absolute" "$destination_path"
 
   # Check if the copy succeeded
   if [ $? -eq 0 ]; then
-      # Check if the file was changed by comparing source and destination
-      if cmp -s "$source_path" "$destination_path"; then
-          # Files are identical, so no change occurred
-          echo "File '$destination_path' was unchanged."
-      else
-          # Files differ, so it was changed
-          echo "File '$destination_path' was changed."
-      fi
+    echo "File '$destination_path' was copied."
   else
-      echo "Error: Failed to copy '$source_path' to '$destination_path'."
+    echo "Error: Failed to copy '$source_path_absolute' to '$destination_path'."
   fi
 done
